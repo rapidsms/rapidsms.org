e9f650d2b9618605f091006347f267d6f7610d49
Rebecca Lovewell <rebecca@caktusgroup.com>
2013-05-21 13:26:45 -0400

    Add dummy static page views

--- website/urls.py
+++ website/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
+from .views import Home, About, Help
+
 
 admin.autodiscover()
 
@@ -13,6 +15,10 @@ urlpatterns = patterns('',
 
 
 urlpatterns += patterns('',
+    url('^$', Home.as_view(), name='home'),
+    url('^about/$', About.as_view(), name='about'),
+    url('^help/$', Help.as_view(), name='help'),
+
     url('^projects/', include('website.projects.urls')),
     url('^packages/', include('website.packages.urls')),
     url('^users/', include('website.users.urls')),
---
+++ website/views.py
@@ -0,0 +1,13 @@
+from django.views.generic import TemplateView
+
+
+class Home(TemplateView):
+    template_name = 'website/home.html'
+
+
+class About(TemplateView):
+    template_name = 'website/about.html'
+
+
+class Help(TemplateView):
+    template_name = 'website/help.html'
