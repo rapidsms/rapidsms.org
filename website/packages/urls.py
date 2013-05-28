7da3c7d525b167ca342ba3583b7a66a528f83972
David Ray <davidray@gmail.com>
2013-05-28 09:58:54 -0400

    work on #14; url validator

--- website/packages/urls.py
+++ website/packages/urls.py
@@ -5,6 +5,6 @@ from .views import PackageAdd, PackageDetail, PackageEdit, PackageList
 urlpatterns = patterns('',
     url('^$', PackageList.as_view(), name='package_list'),
     url('^add/$', PackageAdd.as_view(), name='package_add'),
-    url('^(?P<slug>\w+)/$', PackageDetail.as_view(), name='package_detail'),
-    url('^(?P<slug>\w+)/edit/$', PackageEdit.as_view(), name='package_edit'),
+    url('^(?P<slug>[-\w]+)/$', PackageDetail.as_view(), name='package_detail'),
+    url('^(?P<slug>[-\w]+)/edit/$', PackageEdit.as_view(), name='package_edit'),
 )
