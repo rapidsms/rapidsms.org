86ffec00d82aa18680dae4e06a7ea1b8d5d6af2a
David Ray <davidray@gmail.com>
2013-05-31 11:45:49 -0400

    updating form

--- website/packages/forms.py
+++ website/packages/forms.py
@@ -1,9 +1,20 @@
+import requests
+
 from django import forms
 
 from .models import Package
 
 
 class PackageForm(forms.ModelForm):
-
     class Meta:
         model = Package
+        exclude = ('slug',)
+
+    def clean_pypi_url(self):
+        data = self.cleaned_data['pypi_url']
+        if 'pypi.python.org/pypi' not in data:
+            raise forms.ValidationError("The pypi url must include pypi.python.org/pypi")
+        r = requests.get(data)
+        if r.status_code != '200':
+            raise forms.ValidationError("Could not validate that existence of the URL")
+        return data
