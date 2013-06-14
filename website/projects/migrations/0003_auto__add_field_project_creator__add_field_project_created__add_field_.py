ad862376f8bd13ec44c59ab9753ae9a3cd813702
Colin Copeland <copelco@caktusgroup.com>
2013-06-14 10:01:00 -0400

    migration depends on users initial

--- website/projects/migrations/0003_auto__add_field_project_creator__add_field_project_created__add_field_.py
+++ website/projects/migrations/0003_auto__add_field_project_creator__add_field_project_created__add_field_.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("website.users", "0001_initial"),
+    )
+
     def forwards(self, orm):
         # Adding field 'Project.creator'
         db.add_column(u'projects_project', 'creator',
