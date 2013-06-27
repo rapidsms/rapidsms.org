567f3c6cb3358f0028484289f15e0c8421a759dc
David Ray <davidray@gmail.com>
2013-06-27 07:22:16 -0400

    add add'nl params to secrets for github auth

--- conf/pillar/staging/secrets.ex
+++ conf/pillar/staging/secrets.ex
@@ -1,6 +1,8 @@
 # This file should be renamed to secrets.sls
 secrets:
   DB_PASSWORD: XXXXXX
+  GITHUB_KEY: XXXXXX
+  GITHUB_SECRET: XXXXXX
 
 # Uncomment and update username/password to enable HTTP basic auth
 # http_auth:
