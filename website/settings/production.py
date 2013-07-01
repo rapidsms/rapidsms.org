e66f9fbd237894fa09bfeaea31b33808a606c0ee
Colin Copeland <copelco@caktusgroup.com>
2013-07-01 10:43:16 -0400

    specify path to celery schedule file so it's in a spot with proper permissions

--- website/settings/production.py
+++ website/settings/production.py
@@ -11,3 +11,5 @@ STATIC_ROOT = os.path.join(PUBLIC_ROOT, 'static')
 MEDIA_ROOT = os.path.join(PUBLIC_ROOT, 'media')
 
 EMAIL_SUBJECT_PREFIX = '[Website Prod] '
+
+BROKER_URL = 'amqp://website:%s@127.0.0.1:5672/website_production' % os.environ['BROKER_PASSWORD']
