99040479492ba7c87d1b5d2c79040f5a78ce3135
David Ray <davidray@gmail.com>
2013-07-08 12:13:48 -0400

    rounding out additional Package model tests

--- website/packages/tests/test_models.py
+++ website/packages/tests/test_models.py
@@ -1,7 +1,10 @@
+import datetime
+import json
+
 from website.tests import factories
 from website.tests.base import WebsiteTestBase
 
-from ..models import Package
+from ..models import Package, PYPI_DATE_FORMAT
 from .base import MockPyPIRequest
 
 
@@ -17,6 +20,17 @@ class PackageModelTest(WebsiteTestBase):
         self.assertTrue(updated)
         self.assertEquals(package.version, 'hello')
         self.assertIsNotNone(package.pypi_updated)
+
+    def test_update_from_pypi_200_release_date(self):
+        package = factories.PackageFactory.create()
+        upload_time = datetime.datetime.strftime(datetime.datetime.now(),
+            PYPI_DATE_FORMAT)
+        pypi_request = MockPyPIRequest(status_code=200, upload_time=upload_time)
+        updated = package.update_from_pypi(pypi_request)
+        self.assertTrue(updated)
+        release_date = datetime.datetime.strftime(package.release_date,
+            PYPI_DATE_FORMAT)
+        self.assertEqual(release_date, upload_time)
 
     def test_update_from_pypi_405(self):
         package = factories.PackageFactory.create()
@@ -40,3 +54,9 @@ class PackageModelTest(WebsiteTestBase):
         self.assertEquals(package.slug, '')
         package.save()
         self.assertNotEquals(package.slug, '')
+
+    def test_description(self):
+        descr = 'Foo'
+        data = json.dumps({'info': {'description': descr}})
+        package = factories.PackageFactory.create(pypi_json=data)
+        self.assertIn(descr, package.description)
