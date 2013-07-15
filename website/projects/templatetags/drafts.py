86c2119f0f50914638ba4d31e0d5cfae5f9a7f74
Victor Rocha <victor@rochapps.com>
2013-07-15 12:56:24 -0400

    Corrected failing tests.
    Template tag "count_drafts", "show_drafts" failed when anonymous user
    was passed as an argument.

--- website/projects/templatetags/drafts.py
+++ website/projects/templatetags/drafts.py
@@ -11,12 +11,15 @@ def show_drafts(request):
     "Render template with context variable 'projects'"
     # drafts current user can edit.
     # import pdb; pdb.set_trace()
-    drafts = Project.objects.get_drafts_for_user(request.user)
-    return {"projects": drafts}
+    if request.user.is_authenticated():
+        drafts = Project.objects.get_drafts_for_user(request.user)
+        return {"projects": drafts}
 
 
 @register.assignment_tag
 def count_drafts(request):
     """Returns the number of drafts current user has."""
-    drafts = Project.objects.get_drafts_for_user(request.user)
-    return drafts.count()
+    if request.user.is_authenticated():
+        drafts = Project.objects.get_drafts_for_user(request.user)
+        return drafts.count()
+    return None
