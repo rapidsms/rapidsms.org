49fa9b9377796fdc44bf79bb20c45e6c8ec04048
David Ray <davidray@gmail.com>
2013-07-19 12:32:18 -0400

    Fixed #121, facets abided by in paginator links

--- website/projects/templatetags/facet_tags.py
+++ website/projects/templatetags/facet_tags.py
@@ -31,3 +31,21 @@ def remove_facet(request, facet_value):
                     params[param[0]] = v
     querystring = '?%s' % urlencode(params)
     return {"remove_facet_querystring": querystring}
+
+
+@register.simple_tag
+def faceted_next_prev_querystring(request, page_number):
+    """"Returns a template that provides the URL required to pagination in
+        search results while retaining the selected facets
+
+        Example:
+            {% load facet_tags %}
+            {% faceted_next_prev_querystring request page_number %}
+
+        Renders:
+           ?q=text&page=N&selected_facets=facet:value
+    """
+    q_dict = request.GET.copy()
+    q_dict['page'] = page_number
+    qs = '?%s' % q_dict.urlencode()
+    return qs
--- website/projects/tests/test_templatetags.py
+++ website/projects/tests/test_templatetags.py
@@ -2,9 +2,14 @@ from django.test import RequestFactory
 from django.test import TestCase
 from django.template import Context, Template
 
+from ..templatetags.facet_tags import faceted_next_prev_querystring
+
 
 class FacetTagsTestCase(TestCase):
     """Tests facet_tags.py"""
+
+    def setUp(self):
+        self.request_factory = RequestFactory()
 
     def tag_test(self, template, context, output):
         t = Template('{% load facet_tags %}' + template)
@@ -13,8 +18,7 @@ class FacetTagsTestCase(TestCase):
 
     def test_remove_facet(self):
         """Case where selected_facets is wholly removed"""
-        request_factory = RequestFactory()
-        request = request_factory.get(
+        request = self.request_factory.get(
             '/page',
             data={'q': u'', 'selected_facets': u'facet:Yes'}
         )
@@ -25,8 +29,7 @@ class FacetTagsTestCase(TestCase):
 
     def test_partial_remove_facet(self):
         """Case where 1 of N selected facets is removed"""
-        request_factory = RequestFactory()
-        request = request_factory.get(
+        request = self.request_factory.get(
             '/page',
             data={'q': u'', 'selected_facets': [u'facet:Yes', u'facet1:Blue']}
         )
@@ -34,3 +37,12 @@ class FacetTagsTestCase(TestCase):
         context = {"request": request, "value": 'Blue'}
         output = u'<a href="?q=&amp;selected_facets=facet%3AYes"><i class="icon-remove-sign"></i></a>'
         self.tag_test(template, context, output)
+
+    def test_faceted_next_prev_querystring(self):
+        request = self.request_factory.get(
+            '/page',
+            data={'q': u'', 'selected_facets': u'facet:Yes', 'page': u'1'}
+        )
+        page_number = 5
+        qs = faceted_next_prev_querystring(request, page_number)
+        self.assertIn('page=%s' % page_number, qs)
