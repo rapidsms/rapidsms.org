e59ef49a5f7a3cd990f23f720abdde866476d145
Victor Rocha <victor@rochapps.com>
2013-08-05 08:54:53 -0400

    Take care of handling exceptions in the handle_save method.
    Wrapped up code in a try ... except clause to handle
    possible exceptions.

--- website/index_processors.py
+++ website/index_processors.py
@@ -18,10 +18,34 @@ class BaseSignal(BaseSignalProcessor):
         Given an individual model instance, determine which backends the
         update should be sent to & update the object on those backends.
         """
-        # import pdb; pdb.set_trace()
-        # changed_fields = kwargs['update_fields']
-        # if not changed_fields:
-        #     return False
+        try:
+            # Rises an exception if the status field was not updated.
+            status = kwargs['update_fields']['status']
+            if not instance.status == instance.PUBLISHED:
+                self.remove_from_index(sender, instance, **kwargs)
+        except (TypeError, KeyError):
+            self.add_to_index(sender, instance, **kwargs)
+
+    def handle_delete(self, sender, instance, **kwargs):
+        """
+        Given an individual model instance, determine which backends the
+        delete should be sent to & delete the object on those backends.
+        """
+        using_backends = self.connection_router.for_write(instance=instance)
+        sender = sender if isinstance(instance, sender) else instance.__class__
+        for using in using_backends:
+            try:
+                index = self.connections[using].get_unified_index().get_index(sender)
+                remove_object.delay(index, instance, using=using)
+            except NotHandled:
+                # TODO: Maybe log it or let the exception bubble?
+                pass
+
+    def add_to_index(self, sender, instance, **kwargs):
+        """
+        Given an individual model instance, determine which backends the
+        update should be sent to & update the object on those backends.
+        """
         using_backends = self.connection_router.for_write(instance=instance)
         sender = sender if isinstance(instance, sender) else instance.__class__
         for using in using_backends:
@@ -32,32 +56,12 @@ class BaseSignal(BaseSignalProcessor):
                 # TODO: Maybe log it or let the exception bubble?
                 pass
 
-    def handle_delete(self, sender, instance, **kwargs):
-        """
-        Given an individual model instance, determine which backends the
-        delete should be sent to & delete the object on those backends.
-        """
-        using_backends = self.connection_router.for_write(instance =instance)
-        sender = sender if isinstance(instance, sender) else instance.__class__
-        for using in using_backends:
-            try:
-                index = self.connections[using].get_unified_index().get_index(sender)
-                remove_object.delay(index, instance, using=using)
-            except NotHandled:
-                # TODO: Maybe log it or let the exception bubble?
-                pass
-
-    def removed_unplublished(self, sender, instance, **kwargs):
+    def remove_from_index(self, sender, instance, **kwargs):
         """
         Checks the status of a project and and if it has changed from
         published to any other status removes it from the index.
         """
-        # import pdb; pdb.set_trace()
-        if isinstance(instance, Project):
-            update_fields = kwargs.get('update_fields')
-            if 'status' in update_fields:
-                if not instance.status == instance.PUBLISHED:
-                    self.handle_delete(sender, instance, **kwargs)
+        self.handle_delete(sender, instance, **kwargs)
 
 
 class M2MRealtimeSignalProcessor(BaseSignal):
@@ -67,7 +71,7 @@ class M2MRealtimeSignalProcessor(BaseSignal):
     """
     def setup(self):
         # Naive (listen to all model saves).
-        models.signals.pre_save.connect(self.removed_unplublished)
+        # models.signals.pre_save.connect(self.removed_unplublished)
         models.signals.post_save.connect(self.handle_save)
         models.signals.m2m_changed.connect(self.handle_save)
         models.signals.post_delete.connect(self.handle_delete)
@@ -76,7 +80,7 @@ class M2MRealtimeSignalProcessor(BaseSignal):
 
     def teardown(self):
         # Naive (listen to all model saves).
-        models.signals.pre_save.disconnect(self.removed_unplublished)
+        # models.signals.pre_save.disconnect(self.removed_unplublished)
         models.signals.post_save.disconnect(self.handle_save)
         models.signals.m2m_changed.disconnect(self.handle_save)
         models.signals.post_delete.disconnect(self.handle_delete)
